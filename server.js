4cddaf89f0c36efd224404c5ce04b5d67acf89e1
yangyang861115 <yangyang861115@gmail.com>
2016-02-19 13:02:24 -0500

    make changes in server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 //app.get('/', function(req, res){
 //  res.send('hello world');
 //});
@@ -12,7 +14,7 @@ app.get('/api/users', function(req, res){
   res.json(users);
  });
 
-app.use(express.static(__dirname + '/public'));
+
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
