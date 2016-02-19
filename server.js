a56c7d5b694031047f08a046088b10a06a485147
yangyang861115 <yangyang861115@gmail.com>
2016-02-19 12:48:13 -0500

    changed hello.html

--- server.js
+++ server.js
@@ -5,9 +5,17 @@ var app = express();
 //  res.send('hello world');
 //});
 
+app.get('/api/users', function(req, res){
+  var users = [{username: 'alice', first: 'Alice', last: 'Wonderland'},
+               {username: 'bob', first: 'Bob', last: 'Hope'}
+  ];
+  res.json(users);
+ });
+
 app.use(express.static(__dirname + '/public'));
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
 
 app.listen(port, ipaddress);
+//app.listen(3000);
