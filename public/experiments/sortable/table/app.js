b10110e01cd8cb869083ddfda4b04b512f99f045
yangyang861115 <yangyang861115@gmail.com>
2016-02-29 16:07:01 -0500

    sortable table

--- public/experiments/sortable/table/app.js
+++ public/experiments/sortable/table/app.js
@@ -1,3 +1,16 @@
 /**
  * Created by yangyang on 2/29/16.
  */
+(function(){
+    angular
+        .module("SortableApp", ["jgaSortable"])
+        .controller("SortableController", SortableController);
+
+    function SortableController($scope){
+        $scope.users = [
+            {first: "Alice", last: "wanderland"},
+            {first: "Bob", last: "Hope"},
+            {first: "Charlie", last: "Brown"}
+        ];
+    }
+})();
